b93a2da6505e378f1720b5d1afc38eb4e6ac40ef
Hieunm <hieunm@bytetech.io>
2024-05-24 11:31:57 +0700

    update

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("E:\\personal-standard-ui-custom\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("E:\\personal-standard-ui-custom\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("E:\\personal-standard-ui-custom\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("E:\\personal-standard-ui-custom\\src\\css\\custom.css"),
+];
